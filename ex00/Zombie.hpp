7accb714208a5447b9e82a1267159ffeebd5d366
Abdelkarim Ben el kadi <aben-el-@c1r6p3.1337.ma>
2025-11-24 13:00:44 +0100

    adding changes

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -2,6 +2,23 @@
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
 
+#include <iostream>
 
+class Zombie{
+    private :
+        std::string name;
+    public :
+        void announce(void)
+        {
+            std::cout << name << ":" << ">: BraiiiiiiinnnzzzZ...\n";
+        }
+        Zombie* newZombie(std::string name)
+        {
+
+        }
+        void randomChump(std::string name)
+        {
+        }
+};
 
 #endif
